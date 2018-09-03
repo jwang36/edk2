d5107da1ee0fc8b9ec89103ea27a52923d5b551a
Jian J Wang <jian.j.wang@intel.com>
2018-09-03 09:34:23 +0800

    More gcc build error fixed

--- UefiCpuPkg/CpuMpPei/CpuMpPei.c
+++ UefiCpuPkg/CpuMpPei/CpuMpPei.c
@@ -624,7 +624,7 @@ InitializeMpExceptionStackSwitchHandlers (
 **/
 EFI_STATUS
 InitializeCpuMpWorker (
-  IN CONST EFI_PEI_SERVICES     **PeiServices
+  IN EFI_PEI_SERVICES     **PeiServices
   )
 {
   EFI_STATUS                      Status;
--- UefiCpuPkg/CpuMpPei/CpuMpPei.h
+++ UefiCpuPkg/CpuMpPei/CpuMpPei.h
@@ -411,7 +411,7 @@ SecPlatformInformation2 (
 **/
 EFI_STATUS
 InitializeCpuMpWorker (
-  IN CONST EFI_PEI_SERVICES     **PeiServices
+  IN EFI_PEI_SERVICES     **PeiServices
   );
 
 extern EFI_PEI_NOTIFY_DESCRIPTOR  mPostMemNotifyList[];
