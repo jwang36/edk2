a997444cbe05495946efd14993357618066d8a90
Jian J Wang <jian.j.wang@intel.com>
2018-10-23 20:35:14 +0800

    MdeModulePkg/Core: add freed-memory guard feature
    Freed-memory guard is used to detect UAF (Use-After-Free) memory issue
    which is illegal access to memory which has been freed. The principle
    behind is similar to heap guard feature, that is we'll turn all pool
    memory allocation to page allocation and mark them to be not-present
    once they are freed.
    
    This also implies that, once a page is allocated and freed, it cannot
    be re-allocated. This will bring another issue, which is that there's
    risk that memory space will be used out. To address it, the memory
    service add logic to put part (at most 64 pages a time) of freed pages
    back into page pool, so that the memory service can still have memory
    to allocate, when all memory space have been allocated once. This is
    called memory promotion. The promoted pages are always from the eldest
    pages which haven been freed.
    
    Cc: Star Zeng <star.zeng@intel.com>
    Cc: Michael D Kinney <michael.d.kinney@intel.com>
    Cc: Jiewen Yao <jiewen.yao@intel.com>
    Cc: Ruiyu Ni <ruiyu.ni@intel.com>
    Cc: Laszlo Ersek <lersek@redhat.com>
    Contributed-under: TianoCore Contribution Agreement 1.1
    Signed-off-by: Jian J Wang <jian.j.wang@intel.com>

--- MdeModulePkg/Core/Dxe/Mem/Page.c
+++ MdeModulePkg/Core/Dxe/Mem/Page.c
@@ -401,9 +401,11 @@ PromoteMemoryResource (
   VOID
   )
 {
-  LIST_ENTRY         *Link;
-  EFI_GCD_MAP_ENTRY  *Entry;
-  BOOLEAN            Promoted;
+  LIST_ENTRY            *Link;
+  EFI_GCD_MAP_ENTRY     *Entry;
+  BOOLEAN               Promoted;
+  EFI_PHYSICAL_ADDRESS  StartAddress;
+  EFI_PHYSICAL_ADDRESS  EndAddress;
 
   DEBUG ((DEBUG_PAGE, "Promote the memory resource\n"));
 
@@ -450,6 +452,24 @@ PromoteMemoryResource (
   }
 
   CoreReleaseGcdMemoryLock ();
+
+  if (!Promoted) {
+    //
+    // If freed-memory guard is enabled, we could promote pages from
+    // guarded free pages.
+    //
+    Promoted = PromoteGuardedFreePages (&StartAddress, &EndAddress);
+    if (Promoted) {
+      CoreAcquireGcdMemoryLock ();
+      CoreAddRange (
+        EfiConventionalMemory,
+        StartAddress,
+        EndAddress,
+        EFI_MEMORY_UC | EFI_MEMORY_WC | EFI_MEMORY_WT | EFI_MEMORY_WB
+        );
+      CoreReleaseGcdMemoryLock ();
+    }
+  }
 
   return Promoted;
 }
@@ -896,9 +916,15 @@ CoreConvertPagesEx (
     }
 
     //
-    // Add our new range in
+    // Add our new range in. Don't do this for freed pages if freed-memory
+    // guard is enabled.
     //
-    CoreAddRange (MemType, Start, RangeEnd, Attribute);
+    if (!IsHeapGuardEnabled (GUARD_HEAP_TYPE_FREED) ||
+        !ChangingType ||
+        MemType != EfiConventionalMemory) {
+      CoreAddRange (MemType, Start, RangeEnd, Attribute);
+    }
+
     if (ChangingType && (MemType == EfiConventionalMemory)) {
       //
       // Avoid calling DEBUG_CLEAR_MEMORY() for an address of 0 because this
@@ -1514,6 +1540,7 @@ CoreFreePages (
 
   Status = CoreInternalFreePages (Memory, NumberOfPages, &MemoryType);
   if (!EFI_ERROR (Status)) {
+    GuardFreedPagesChecked (Memory, NumberOfPages);
     CoreUpdateProfile (
       (EFI_PHYSICAL_ADDRESS) (UINTN) RETURN_ADDRESS (0),
       MemoryProfileActionFreePages,
@@ -1908,9 +1935,7 @@ Done:
   *MemoryMapSize = BufferSize;
 
   DEBUG_CODE (
-    if (PcdGet8 (PcdHeapGuardPropertyMask) & (BIT1|BIT0)) {
-      DumpGuardedMemoryBitmap ();
-    }
+    DumpGuardedMemoryBitmap ();
   );
 
   return Status;
